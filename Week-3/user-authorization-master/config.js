87210ffcbec2cbaa295e2ab0906b604ad41dca4e
Hugo Andrés Machorro Meléndez <hugo.machorro@axity.com>
2023-10-05 00:32:21 -0600

    [ADDED] chanllenge week-3 50/100

---
+++ Week-3/user-authorization-master/config.js
@@ -0,0 +1,8 @@
+const config = {
+  PORT: process.env.PORT || "3000",
+  CLIENT_ID: process.env.CLIENT_ID || "aa7956e5edd85e60f088",
+  CLIENT_SECRET:
+    process.env.CLIENT_SECRET || "cbb4a0648c59ff8304bb1b429518ae4cba6ccbf7",
+};
+
+module.exports = config;
